7a7c704199465a8439d0f6e57329bc46f1e19ed0
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-03-15 10:56:33 +0700

    added store method (create feature)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,21 +6,39 @@ use App\Http\Resources\PostDetailResource;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $posts = Post::all();
         // return response()->json(['data' => $posts]);
         return PostResource::collection($posts);
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $post = Post::with('writer:id,username')->findOrFail($id);
         return new PostDetailResource($post);
     }
-    public function show2($id){
+
+    public function show2($id)
+    {
         $post = Post::findOrFail($id);
         return new PostDetailResource($post);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'news_content' => 'required'
+        ]);
+        // return response()->json('Sudah dapat digunakan');
+
+        $request['author'] = Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
     }
 }
