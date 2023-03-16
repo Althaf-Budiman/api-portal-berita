48c847d485cbc48de08db6f335f8c58c02aef5f8
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-03-16 14:52:17 +0700

    create comment middleware

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -23,4 +23,11 @@ class CommentController extends Controller
 
         return new CommentResource($comment->loadMissing(['commentator:id,username']));
     }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            
+        ]);
+    }
 }
